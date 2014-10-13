64377b38e7caee044464f3d06471449ecd71d900
BeshoSmile <beshosmile@gmail.com>
2014-10-13 17:29:13 +0200

    Add static files

--- facebook_example/member/views.py
+++ facebook_example/member/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+from django.conf import settings
+from django.contrib import messages
+from django.http import Http404
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django_facebook import exceptions as facebook_exceptions, \
+    settings as facebook_settings
+from django_facebook.connect import CONNECT_ACTIONS, connect_user
+from django_facebook.decorators import facebook_required_lazy
+from django_facebook.utils import next_redirect, get_registration_backend, \
+    to_bool, error_next_redirect, get_instance_for
+from open_facebook import exceptions as open_facebook_exceptions
+from open_facebook.utils import send_warning
+import logging
+
+def example(request):
+    context = RequestContext(request)
+    return render_to_response('example.html', context)
